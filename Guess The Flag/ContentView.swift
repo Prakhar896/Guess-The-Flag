e4cd4b999de51b5ef24a1d7501136b3fc4c8b5b2
Prakhar Trivedi <prakh0706@outlook.com>
2021-11-16 22:11:06 +0800

    Initial Commit

---
+++ Guess The Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess The Flag
+//
+//  Created by Prakhar Trivedi on 16/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
